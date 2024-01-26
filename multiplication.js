036eefedf12b6e2a5e29a25c85eb5ac3e6d8d64e
yasshh17 <111655822+yasshh17@users.noreply.github.com>
2024-01-25 22:07:46 -0800

    A math website: Module 2 Assignment

---
+++ multiplication.js
@@ -0,0 +1,8 @@
+function performMultiplication() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+
+    const result = num1 * num2;
+
+    document.getElementById('result').innerText = `Result: ${result}`;
+}
